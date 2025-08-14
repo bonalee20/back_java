d67bc1a1540a9ac19111fc86a72635654fa7bfbe
bonalee20 <bboonnaaa0226@gmail.com>
2025-08-14 17:45:10 +0900

    day01

---
+++ variable/src/variableTest/VariableTask01.java
@@ -0,0 +1,31 @@
+package variableTest;
+//실습
+//각 데이터에 맞는 타입 선언 후 출력해보기
+//5, false, 7845.15, B, 85.10F, 자바, S, 98590, D, true, 107L 공백문자
+
+public class VariableTask01 {
+	public static void main(String[] args) {
+		
+		int num1 = 5, num4 = 98590;
+		boolean question = false, question2 = true;
+		double num2 = 7845.15;
+		float num3 = 85.10F;
+		char name1 = 'B', name2 = 'S', name3 = 'D', name6 = ' ';
+		String name4="자바,", name5="107L,";
+		
+		System.out.println(num1);
+		System.out.println(question);
+		System.out.println(num2);
+		System.out.println(name1);
+		System.out.println(num3);
+		System.out.println(name4);
+		System.out.println(name2);
+		System.out.println(num4);
+		System.out.println(name3);
+		System.out.print(question2);
+		System.out.print(name5+',');
+		System.out.println(name6);
+	
+
+	}
+}
---
+++ variable/src/variableTest/VariableTest.java
@@ -0,0 +1,19 @@
+package variableTest;
+
+public class VariableTest {
+	public static void main(String[] args) {
+		String name = "홍길동";
+//		초기값 변수를 비우자
+//		정수(int): 0
+//		실수(double): 0.0
+//		문자(char): ' ' --> 여기까지 기본 자료형 
+		
+//		문자열(String): null, "" -> 클래스형 자료형
+		int num1 = 10;
+		double num2 = 10.7;
+		float num3 = 10.7F;
+		String hello = "안녕";
+		char data = '가';
+		long num4 = 10L;
+	}
+}
